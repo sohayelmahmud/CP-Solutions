107e58674ae64da1482697e9edd31c7787f4d6e8
sohayelmahmud <sohayelmahmud612@gmail.com>
2026-06-23 02:38:59 +0500

    day 21

---
+++ cf-90-days/B_Cake_Leveling.cpp
@@ -0,0 +1,103 @@
+// g++ Triple_Xor.cpp -o Triple_Xor && ./Triple_Xor < input.txt
+#include <bits/stdc++.h>
+using namespace std;
+// clang-format off
+// Typedefs & Defines
+typedef long long           ll;
+typedef long double         ld;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef pair<ll, ll>        pii;
+
+#define elif                else if
+#define f0(i, n)            for (ll i = 0; i < (n); i++)
+#define f1(i, n)            for (ll i = 1; i <= (n); i++)
+
+#define all(x)              (x).begin(), (x).end()
+#define rall(x)             (x).rbegin(), (x).rend()
+#define len(a)              ((int64_t)(a).size())
+#define ff                  first
+#define ss                  second
+#define pb                  push_back
+
+#define YES                 cout << "YES" << endl
+#define NO                  cout << "NO" << endl
+#define Yes                 cout << "Yes" << endl
+#define No                  cout << "No" << endl
+#define yes                 cout << "yes" << endl
+#define no                  cout << "no" << endl
+
+#define min_arr(a, n)       *min_element(a, a + n)
+#define max_arr(a, n)       *max_element(a, a + n)
+#define min_vec(v)          *min_element(all(v))
+#define max_vec(v)          *max_element(all(v))
+
+template<typename dataType>
+inline dataType abs(dataType k) {if (k >= 0) return k; else return (-k);}
+template<typename dataType>
+inline bool isEqual(dataType a, dataType b) {return (abs((dataType)(a-b)) < 1e-9);}
+// ############ ---- Input Functions ---- ############------------
+#define inin(n)             ll n; cin >> n
+#define insr(s)             string s; cin >> s
+#define inch(c)             char c; cin >> c
+#define inin2(a,b)          ll a,b; cin >> a >> b
+#define inarr(arr,n)        ll arr[n]; f0(t_ind, n) cin >> arr[t_ind]
+#define varr(v, n)          vll v(n); f0(i, n) cin >> v[i]
+template <typename... T> //--------------- multiple variable input
+void invr(T &...args)
+{                         //-------------- int x, y, z;
+    (cin >> ... >> args); // ------------- invr(x, y, z);
+} //---------------------------------------------------------------
+vector<int> inlt(ll n) //------------- List int input
+{
+    vector<int> arr(n);         //----- int size = 5;
+    for (ll i = 0; i < n; i++) //----- vector<int> a = inlt(size);
+        cin >> arr[i];
+    return arr;
+} //---------------------------------------------------------------
+// ############ ---- Constants ---- ############
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+// ############ ---- Useful Funtions ---- ############
+
+// clang-format on
+// ############ ---- Main Solve Function ---- ############
+void solve(int testNo)
+{
+    inin(n);
+    varr(arr, n);
+    ll ans = INF, sum = 0;
+    f0(i, n)
+    {
+        sum += arr[i];
+        ans = min(ans, sum / (i + 1));
+        cout << ans << " ";
+    }
+}
+
+void solve2(int testNo)
+{
+    // cout << "Case #" << testNo << ": ";
+}
+
+void solve3(int testNo)
+{
+    // cout << "Case #" << testNo << ": ";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+
+    cin >> t;
+    for (int testNo = 1; testNo <= t; testNo++)
+    {
+        solve(testNo);
+        cout << endl;
+    }
+
+    return 0;
+}
